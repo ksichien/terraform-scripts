fd5225f9188170870a1c7155628a4af562b643f5
ksichien <ksichien@posteo.net>
2018-02-15 15:15:09 +0100

    initial commit

---
+++ azure/main.tf
@@ -0,0 +1,7 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    subscription_id = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    client_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    client_secret   = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    tenant_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# terraform-scripts
+This is a small collection of Terraform scripts I've written to make life easier for myself.
