36eb90a8940d8564c947d1a10139f2ee4dee69fb
ksichien <ksichien@posteo.net>
2018-02-15 17:39:05 +0100

    added azure script

---
+++ azure/create-linux-vm.tf
@@ -0,0 +1,9 @@
+# https://docs.microsoft.com/en-us/azure/virtual-machines/linux/terraform-create-complete-vm
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    subscription_id = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    client_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    client_secret   = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    tenant_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
